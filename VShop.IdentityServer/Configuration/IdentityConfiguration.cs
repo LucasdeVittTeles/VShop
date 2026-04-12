92a9d1709f7d2bc6e91c4e40abf2b26b285f1896
Lucas <lucasdevittteles@gmail.com>
2026-04-12 17:05:55 -0300

    salvando

---
+++ VShop.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace VShop.IdentityServer.Configuration
+{
+    public class IdentityConfiguration
+    {
+
+        public const string Admin = "Admin";
+        public const string Client = "Client";
+
+        public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile(),
+        };
+
+        //public static IEnumerable<IdentAityResource> ApiScopes => new List<IdentityResource>
+        //{
+        //    new IdentityResources.OpenId(),
+        //    new IdentityResources.Email(),
+        //    new IdentityResources.Profile(),
+        //};
+
+
+    }
+}
