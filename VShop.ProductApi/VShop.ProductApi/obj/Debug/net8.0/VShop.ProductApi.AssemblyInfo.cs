9abbcaf993b6767883cafd774bd8b07e6877db65
Lucas <lucasdevittteles@gmail.com>
2026-05-26 13:46:18 -0300

    salvando novo layout de telas.

--- VShop.ProductApi/VShop.ProductApi/obj/Debug/net8.0/VShop.ProductApi.AssemblyInfo.cs
+++ VShop.ProductApi/VShop.ProductApi/obj/Debug/net8.0/VShop.ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VShop.ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d09489bd2de1b7ab5282efc7d6b8fcb7d8c17c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+099a6baa4c5ff2ce6c0fea932d30a24d5648f7b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("VShop.ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VShop.ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
